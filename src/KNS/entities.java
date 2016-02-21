1fa8314997fd08caec7d62c33120f8000e0e44c0
kodoo <bm.kodoo@gmail.com>
2016-02-21 14:53:38 +0300

    transform matrices

---
+++ src/KNS/entities.java
@@ -0,0 +1,7 @@
+package KNS;
+
+/**
+ * Created by kodoo on 21.02.2016.
+ */
+public class entities {
+}
--- src/KNS/shaders/StaticShader.java
+++ src/KNS/shaders/StaticShader.java
@@ -1,6 +1,7 @@
 package KNS.shaders;
 
 import KNS.shaders.ShaderProgram;
+import org.lwjgl.util.vector.Matrix4f;
 
 /**
  * Created by kodoo on 15.02.2016.
@@ -9,19 +10,24 @@ public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "shader.vert";
     private static final String FRAGMENT_FILE = "shader.frag";
+    private int location_transformationMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
     }
 
     @Override
-    protected void getAllUniformLocations() {
-
-    }
-
-    @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
         super.bindAttribute(1, "textureCoords");
+    }
+
+    @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformmationMatrix");
+    }
+
+    public void loadtransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_transformationMatrix, matrix);
     }
 }

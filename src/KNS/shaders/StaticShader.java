68c1aa09115393626829c117a0f1f28b12ede191
kodoo <bm.kodoo@gmail.com>
2016-03-13 10:46:01 +0300

    view matrix to StaticShader class

--- src/KNS/shaders/StaticShader.java
+++ src/KNS/shaders/StaticShader.java
@@ -1,5 +1,7 @@
 package KNS.shaders;
 
+import KNS.entities.Camera;
+import KNS.toolbox.Maths;
 import org.lwjgl.util.vector.Matrix4f;
 
 /**
@@ -11,9 +13,14 @@ public class StaticShader extends ShaderProgram {
     private static final String FRAGMENT_FILE = "shader.frag";
     private int location_transformationMatrix;
     private int location_projectionMatrix;
+    private int location_viewMatrix;
 
     public StaticShader() {
         super(VERTEX_FILE, FRAGMENT_FILE);
+    }
+
+    public StaticShader(String vertexFile, String fragmentFile) {
+        super(vertexFile, fragmentFile);
     }
 
     @Override
@@ -26,10 +33,16 @@ public class StaticShader extends ShaderProgram {
     protected void getAllUniformLocations() {
         location_transformationMatrix = super.getUniformLocation("transformmationMatrix");
         location_projectionMatrix = super.getUniformLocation("projectionMatrix");
+        location_viewMatrix = super.getUniformLocation("viewMatrix");
     }
 
     public void loadTransformationMatrix(Matrix4f matrix) {
         super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
+    public void loadViewMatrix(Camera camera) {
+        Matrix4f viewMatrix = Maths.createViewMatrix(camera);
+        super.loadMatrix(location_viewMatrix, viewMatrix);
     }
 
     public void loadProjectionMatrix(Matrix4f matrix) {
